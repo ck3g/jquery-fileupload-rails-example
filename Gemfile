0fce3e31f9581b3ecf78be6c252fd358cbe0d408
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2013-09-15 12:28:41 +0300

    Initial (configure rails)

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.0'
+
+gem 'pg'
+
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+
+# See https://github.com/sstephenson/execjs#readme for more supported runtimes
+# gem 'therubyracer', platforms: :ruby
+
+gem 'jquery-rails'
+gem 'turbolinks'
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JqueryFileuploadRailsExample::Application.config.secret_key_base = '7eabf48a3126d6ba4a0c8c87967ef19c37bebcfcba581a6ce9b7a466a4663b4fa05cc7de28fda86bdea169dd1e892f0925f74f79c53e185a12fa73aa655bb2c2'
